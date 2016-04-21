1aa5ffebc1051371a9a1b07f5985c0bc38ea09fe
grenadesonfire <nickybrianc@gmail.com>
2016-04-21 16:53:40 -0400

    Began the Parser
    This currently just has placeholder code

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Compiler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Wonderfully simple Logging.
+        public static readonly log4net.ILog log = log4net.LogManager.GetLogger(
+            System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        //Processing elements
+        Tokenizer _tokenizer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeProcessingElements();
+        }
+
+        private void InitializeProcessingElements()
+        {
+            _tokenizer = new Tokenizer();
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Jack Files | *.jack";
+            if (ofd.ShowDialog() == true)
+            {
+                JackFile_TextBox.Text = File.ReadAllText(ofd.FileName);
+            }
+        }
+
+        private void Tokenize_Click(object sender, RoutedEventArgs e)
+        {
+            _tokenizer.Process(JackFile_TextBox.Text);
+        }
+    }
+}
---
+++ Tokenizer.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler
+{
+    class Token
+    {
+        enum TokenType
+        {
+            KEYWORD,
+            SYMBOL,
+            IDENTIFIER,
+            INT_CONST,
+            STRING_CONST
+        }
+        enum KeyWordType
+        {
+            CLASS,
+            METHOD,
+            FUNCTION,
+            CONSTRUCTOR,
+            INT,
+            BOOLEAN,
+            CHAR,
+            VOID,
+            VAR,
+            STATIC,
+            FIELD,
+            LET,
+            DO,
+            IF,
+            ELSE,
+            WHILE,
+            RETURN,
+            TRUE,
+            FALSE,
+            NULL,
+            THIS
+        }
+    }
+    internal class Tokenizer
+    {
+        private string originalFile { get; set; }
+        public List<string> _output;
+        internal void Process(string text)
+        {
+            originalFile = text;
+            _output.Clear();
+            //string[] tmpLines = originalFile.Split(new string[] { "\r","\n"},StringSplitOptions.RemoveEmptyEntries);
+            //foreach(string s in tmpLines)
+            //{
+            ParseLine(text,0);
+            //}
+        }
+
+        private void ParseLine(string s,int start)
+        {
+            for(int i = start; i < s.Length; i++)
+            {
+                /*sb.Append(s[i]);
+                if(sb.ToString() == "//")
+                {
+                    ParseLineComment(s, start + i+1, out i);
+                }*/
+
+                //We see a letter character
+                if (char.IsLetter(s[i]))
+                {
+                    ParseWord(s, start, out i);
+                }
+                //We see a number character
+                else if (char.IsNumber(s[i]))
+                {
+                    ParseNumber(s, start,out i);
+                }
+                else
+                {
+                    ParseSymbol(s, start, out i);
+                }
+            }
+        }
+
+        private void ParseSymbol(string s, int start, out int i)
+        {
+            i = start + 1;
+        }
+
+        private void ParseNumber(string s, int start,out int i)
+        {
+            i = start + 1;
+        }
+
+        private void ParseWord(string s, int start,out int i)
+        {
+            i = start + 1;
+            if(s[start] == '/' && s[start+1] == '/')
+            {
+                ConsumeUntilNewline(s,start+2,out i);
+            }
+        }
+
+        private void ConsumeUntilNewline(string s, int start,out int end)
+        {
+            end = start;
+            while (end < s.Length && s[end] != '\n' && s[end] != '\n')
+            {
+                end++;
+            }
+            end++;
+        }
+    }
+}
